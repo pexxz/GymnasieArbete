88722a9df4cec77d5dc0ae988061ab046f8f7dc8
pexxz <pexxz_98@hotmail.com>
2020-05-06 02:33:30 +0200

    Update Respawn.cs

--- Respawn.cs
+++ Respawn.cs
@@ -6,10 +6,9 @@ public class Respawn : MonoBehaviour
     [SerializeField] private Transform respawnPoint;
 
 
-    //If the player collides with Respawn trigger, teleports player to respawnPoint
+    //If player collides with Respawn trigger, teleports player to respawnPoint
     private void OnTriggerEnter2D(Collider2D collision)
     {
         player.transform.position = respawnPoint.transform.position;
     }
-
 }
